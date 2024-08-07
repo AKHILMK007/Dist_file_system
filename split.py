05e7fb62b3ffd0e24982d3306ce3c35b0b60a107
Dhruva <dhruvanayak1905@gmail.com>
2024-08-07 14:04:18 +0530

    add split.py

---
+++ split.py
@@ -0,0 +1,26 @@
+import os
+import json
+import math
+
+def split(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+        
+        total_length = len(file_content)
+        chunk_size = math.ceil(total_length / 3)
+        
+        chunks = [file_content[i:i + chunk_size] for i in range(0, total_length, chunk_size)]
+        
+        json_data = {}
+        base_name = os.path.basename(file_path).split('.')[0]
+        
+        for i, chunk in enumerate(chunks):
+            json_data[f"{base_name}_{i+1}"] = chunk
+        
+        return json_data
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
